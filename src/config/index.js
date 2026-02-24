03b1d7b8c6c1ef62d034db16b7f6627ca8bcc907
KingLiodas <hirokikomatsu0@gmail.com>
2026-02-24 16:17:29 +0800

    feat: add pokemon repository for API calls
    Full Name: Hiroki Komatsu
    Umindanao: h.komatsu.540176@umindanao.edu.ph

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,4 @@
 import dotenv from 'dotenv';
-import { Router } from 'express';
-import pokemonRoutes from './pokemonRoutes.js';
-
-const router = Router();
-
-// Mount all Pokemon routes at root
-router.use('/', pokemonRoutes);
-
-export default router;
 
 
 
